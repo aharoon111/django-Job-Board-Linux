dc7c71b6cd288ee66dd72e793cbbeb801b2bfd02
Ahmed Haroon <aharoon@192.168.1.25>
2020-08-28 17:54:23 +0200

    add job views

--- job/models.py
+++ job/models.py
@@ -11,15 +11,15 @@ JOB_TYPE = (
 
 
 
-class Job(models.Model):
-    title = models.CharField(max_length=100)
+class Job(models.Model):                              #table
+    title = models.CharField(max_length=100)          #column
     #location
     job_type = models.CharField(max_length= 15, choices = JOB_TYPE)
     description = models.TextField(max_length =1000 )
     published_at = models.DateTimeField(auto_now= True)
     vacancy = models.IntegerField(default = 1)
     salary = models.IntegerField(default = 0)
-    #category 
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
     Experience = models.IntegerField(default = 1)
 
 
@@ -27,3 +27,8 @@ class Job(models.Model):
         return self.title
 
 
+class Category(models.Model):
+    name = models.CharField(max_length= 25)
+
+    def __str__(self):
+        return self.name
