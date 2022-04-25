3de3daf535f76933ed02a19532567c0b53a9d13c
Ahmed Haroon <aharoon111@gmail.com>
2022-04-25 14:59:48 +0200

    template job list , details edits

--- job/urls.py
+++ job/urls.py
@@ -1,9 +1,10 @@
- 
-from django.urls import path , include
+from django.urls import path, include
 from . import views
 
-urlpatterns = [   
-    
-    path('',views.job_list),
-    path('<int:id>',views.job_details)
+app_name = 'job'
+
+urlpatterns = [
+
+    path('', views.job_list),
+    path('<int:id>', views.job_details , name='job_details')
 ]
